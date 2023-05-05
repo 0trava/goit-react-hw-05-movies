4da93bb5d6f1d8a5c9bce87b8aa63e61d95c4718
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-05 12:45:09 +0300

    add style

--- src/pages/Movies/MovieDetails/MovieDetails.jsx
+++ src/pages/Movies/MovieDetails/MovieDetails.jsx
@@ -9,20 +9,24 @@ const MovieDetails = (id) => {
         <button className={css.movieDet_btn}>&#129032; Go back</button>
         <div className={css.movieDet_block}>
             <img src="https://harrypotter.org.ua/wa-data/public/shop/products/54/53/25354/images/38982/38982.750x0.jpg" alt="" />
-            <div>
-                <h2>Title</h2>
-                <p><span>User Score:</span> 50% </p>
-                <h3>Overview</h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-                <h3>Genres</h3>
-                <p>Drama, History, War</p>
+            <div className={css.movieDet_infoBlock}>
+                <h2 className={css.movieDet_title}>Title</h2>
+                <p className={css.movieDet_userScore}><span>User Score:</span> 50% </p>
+                <h3 className={css.movieDet_pre_title}>Overview</h3>
+                <p className={css.movieDet_text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <h3 className={css.movieDet_pre_title}>Genres</h3>
+                <p className={css.movieDet_text}>Drama, History, War</p>
             </div>
         </div>
-        <h3>Additional information</h3>
-        <ul>
-            <li><link to =""/>Cast</li>
-            <li><link to ="" />Reviews</li>
-        </ul>
+            <div className={css.movieDet_additional_block}>
+                <hr className={css.movieDet_line} />
+                <h3 className={css.movieDet_pre_title}>Additional information</h3>
+                <ul>
+                    <li><link to =""/>Cast</li>
+                    <li><link to ="" />Reviews</li>
+                </ul>
+            </div>
+
         <Outlet/>
 
      </>
