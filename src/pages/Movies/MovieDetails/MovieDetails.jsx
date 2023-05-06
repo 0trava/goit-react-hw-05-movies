046f069d005b16efb0e9f101f6482e4f0e6b0dd1
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-06 20:58:47 +0300

    add movie details

--- src/pages/Movies/MovieDetails/MovieDetails.jsx
+++ src/pages/Movies/MovieDetails/MovieDetails.jsx
@@ -1,39 +1,80 @@
-import { Outlet } from 'react-router-dom';
 import css from "./MovieDetails.module.css";
+import Loader from '../../../components/Loader/Loader';
+import { useState, useEffect, lazy, Suspense } from 'react';
+import { getMovieDetails, IMAGE_URL } from '../../../services/movies-api';
+import {
+    Route,
+    Routes,
+    useParams,
+    Outlet,
+    Link,
+    // useMatch,
+    // useLocation,
+    // useNavigate,
+  } from 'react-router-dom';
 
 
-const MovieDetails = (id) => {
+const MovieReview = lazy(() =>import('../Reviews/Reviews' /* webpackChunkName:"MovieReview" */));
+const MovieCastView = lazy(() =>import('../cast/Cast' /* webpackChunkName:"MovieCastView" */));
+
+export default function MovieDetailsPage() {
+    const [movie, setMovie] = useState(null);
+    const { movieId } = useParams();
+    // const history = useNavigate();
+    // const location = useLocation();
+    // const { url, path } = useMatch();
     console.log("start");
+
+
+    useEffect(() => {
+        const getMovie = async () => {
+          const currentMovie = await getMovieDetails(movieId);
+          setMovie(currentMovie);
+        };
+    
+        getMovie();
+      }, [movieId]);
+
+
+    //   const onGoBack = () => {
+    //     history.push(location?.state?.from?.location ?? '/movies');
+    //   };
+
     return (
-     <>
-        <button className={css.movieDet_btn}>&#129032; Go back</button>
+    <div  className={css.movieDet_container}>
+    {!movie ? (
+    <div>This movie is not found</div>
+    ) : (
+        <>
+        <button  
+        // onClick={onGoBack} 
+        className={css.movieDet_btn}>&#129032; Go back</button>
         <div className={css.movieDet_block}>
-            <img src="https://harrypotter.org.ua/wa-data/public/shop/products/54/53/25354/images/38982/38982.750x0.jpg" alt="" />
+            <img src={ movie.poster_path
+                       ? IMAGE_URL + movie.poster_path
+                       : `https://bitsofco.de/content/images/2018/12/broken-1.png`}
+                       alt={movie.title} />
             <div className={css.movieDet_infoBlock}>
-                <h2 className={css.movieDet_title}>Title</h2>
-                <p className={css.movieDet_userScore}><span>User Score:</span> 50% </p>
+                <h2 className={css.movieDet_title}>{movie.title}</h2>
+                <p className={css.movieDet_userScore}>User Score: {`${movie.vote_average * 10}`}% </p>
                 <h3 className={css.movieDet_pre_title}>Overview</h3>
-                <p className={css.movieDet_text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p className={css.movieDet_text}>{`${movie.overview}`}</p>
                 <h3 className={css.movieDet_pre_title}>Genres</h3>
-                <p className={css.movieDet_text}>Drama, History, War</p>
+                <p className={css.movieDet_text}>{`${movie.genres.map(genre => genre.name).join(' / ')}`}</p>
             </div>
         </div>
+        </>
+
+    )}
             <div className={css.movieDet_additional_block}>
                 <hr className={css.movieDet_line} />
                 <h3 className={css.movieDet_pre_title}>Additional information</h3>
                 <ul className={css.movieDet_link}>
-                    <li><link to =""/>Cast</li>
-                    <li><link to ="" />Reviews</li>
+                    <li><Link/>Cast</li>
+                    <li><Link />Reviews</li>
                 </ul>
             </div>
 
-        <Outlet/>
-
-     </>
-
-    )
-
-
-}
-
-export default MovieDetails;
+                <Outlet/>
+    </div>
+    )}
