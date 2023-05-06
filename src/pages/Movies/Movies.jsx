63490b5d7f50af6ec2288a5937ef532263604929
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-05-07 00:17:59 +0300

    add search params

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,21 +1,45 @@
 import css from './Movies.module.css';
 import {useState } from 'react'; // пакети для роботи зі станом
-import {Link} from 'react-router-dom';
+import {Link, useSearchParams} from 'react-router-dom';
+import {searchMovies} from '../../services/movies-api';
 
 
 
 const Movies = () => {
 const [filter, setFilter] = useState(''); // Хук для filter
+const [searchParams, setSearchParams] = useSearchParams();
+const [listMovies, setListMovies] = useState();
+
+
+const getSearchParams = (e) => {
+
+    if (filter === ""){
+        setSearchParams({});
+    } else {
+        e.preventDefault(); // Зупиняємо оновлення сторінки
+        let filmlist = searchMovies(filter);
+        setSearchParams({c:`${filter}`});
+        setFilter("");
+        console.log(filmlist);
+    
+        setListMovies(filmlist);
+    }
+
+
+}
+
+
 
 const handleChange = (event) => {
-    setFilter(event.currentTarget.value)
+    setFilter(event.currentTarget.value);
 }
 
 return (
-    <div className={css.Movies_container}>
+    <>
+        <div className={css.Movies_container}>
          <form className={css.SearchForm}>
           <Link> 
-          <button type="submit" className={css.SearchForm_button}>
+          <button type="submit" className={css.SearchForm_button} onClick={getSearchParams}>
             <span className={css.SearchForm_button_label}>Search</span>
           </button>
           </Link> 
@@ -31,7 +55,15 @@ return (
             placeholder="Search images and photos"
           />
         </form>
-    </div>
+        </div>
+
+        { setListMovies && (
+            <>
+            <p>Nen</p>
+            </>
+        ) }
+    </>
+
 
 )    
 }
